0b45fc740159d048d798159f709e29a05614245e
KhalilouBacongo <khaliloubacongo@gmail.com>
2025-11-12 11:02:34 +0000

    ✨ Dashboard Admit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -77,7 +77,7 @@ const Header = () => {
               shadow-md hover:shadow-xl hover:scale-105 transition-all duration-200"
           >
             <UserPlus size={18} />
-            S'inscrire
+            Être membre
           </NavLink>
         </nav>
       </div>
